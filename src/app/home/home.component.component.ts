e1077fb03746a387c9879358f323fe82a086a02e
Walter <wcmore.93@gmsil.com>
2024-05-11 08:54:49 -0500

    Se agrego en el home , una prueba de envio de sms

--- src/app/home/home.component.component.ts
+++ src/app/home/home.component.component.ts
@@ -1,11 +1,14 @@
 import { Component,  ElementRef, Renderer2 } from '@angular/core';
 import { Resend } from 'resend';
-import { EmailService } from '../email.service';
 import Swal from 'sweetalert2';
 import { Donante } from '../MantenerClientes/donante.model';
 import { DonanteService } from '../MantenerClientes/donante.service';
 import { AppComponent } from '../app.component';
 import { Router } from '@angular/router';
+import { HttpClient, HttpErrorResponse, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+
 
 
 
@@ -18,20 +21,64 @@ import { Router } from '@angular/router';
 export class HomeComponentComponent {
 
   constructor(
-    private donanteService : DonanteService, private elRef: ElementRef, private renderer: Renderer2, private router: Router){}
+    private donanteService : DonanteService, private elRef: ElementRef, private renderer: Renderer2, private router: Router,
+    private http: HttpClient){}
 
-  donanteLst: Donante[] = [];
+    toNumber: string = '+51962303062';
+    message: string = 'Gracias por su registro, bienvenido a Alimenta y Comparte';
 
-  titulo: string = 'MANTENIMIENTO DE DONANTES';
+        
+    donanteLst: Donante[] = [];
 
-  donante_id_input: number = 0;
-  tipo_donante: number =  0;
-  tipo_documento: number = 0;
-  nrodocumentoinput: string='';
-  nombreInput: string = '';
-  direccionInput: string ='';
-  telefonoInput: string = '';
-  emailInput: string = '';
+    titulo: string = 'MANTENIMIENTO DE DONANTES';
+
+    donante_id_input: number = 0;
+    tipo_donante: number =  0;
+    tipo_documento: number = 0;
+    nrodocumentoinput: string='';
+    nombreInput: string = '';
+    direccionInput: string ='';
+    telefonoInput: string = '';
+    emailInput: string = '';
+
+    enviarMensaje() {
+      const url = 'https://api.twilio.com/2010-04-01/Accounts/ACb4b8a6604a46ff1a14b0e66fb6415c5c/Messages.json';
+      const usuario = 'ACb4b8a6604a46ff1a14b0e66fb6415c5c';
+      const contraseña = '5eaf97999f755b86a5201885d082f9eb';
+  
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(`${usuario}:${contraseña}`)
+      });
+  
+      const body = new URLSearchParams();
+      body.set('To', '+51962303062');
+      body.set('From', '+13253123628');
+      //body.set('Body', 'Gracias por su registro, bienvenido a Alimenta y Comparte ,Su usurio es: donante, pass: 1234');
+      const mensaje = 'Gracias por su registro, bienvenido a Alimenta y Comparte.\nSu usuario es: donante\nPass: 1234';
+      body.set('Body', mensaje);
+
+  
+      this.http.post(url, body.toString(), { headers: headers })
+        .pipe(
+          catchError((error: HttpErrorResponse) => {
+            console.error('Ocurrió un error:', error);
+            console.error('Nombre del error:', error.name);
+            console.error('Mensaje del error:', error.message);
+            console.error('Código de estado HTTP:', error.status);
+            console.error('Cuerpo del error:', error.error);
+            return throwError(error); // Propaga el error
+          })
+        )
+        .subscribe(
+          response => {
+            console.log('Mensaje enviado correctamente:', response);
+            // Maneja la respuesta según tus necesidades
+          }
+        );
+    }
+
+
   
 
 
@@ -72,6 +119,7 @@ export class HomeComponentComponent {
         { id:3 , nombre: 'CE'}
       ]; // Lista de opciones para el combobox
      // Variable para almacenar el tipo seleccionado
+
 
 
     agregarDonante(){
